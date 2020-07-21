0b08e326b78a28308092fe9a076136badd412c8e
eshc@naver.com <tjrwns@23>
2020-07-21 23:35:47 +0900

    1251 clear

--- baekjoon/1251_baekjoon.py
+++ baekjoon/1251_baekjoon.py
@@ -1,2 +1,12 @@
-s = input()
-
+s = list(input())
+l = len(s)
+m='z'*l
+for i in range(1,l):
+    for j in range(i+1,l):
+        a,b,c = s[:i],s[i:j],s[j:]
+        a.reverse()
+        b.reverse()
+        c.reverse()
+        if ('').join(a+b+c) < m:
+            m=('').join(a+b+c)
+print(m)
