8844c385ea86465372dfe2cbe50be33b7ac53a70
eshc123 <eshc@naver.com>
2020-07-29 12:11:24 +0900

    9095 brute force clear

--- baekjoon/9095_baekjoon.py
+++ baekjoon/9095_baekjoon.py
@@ -1,3 +1,25 @@
+# 재귀
+def one_two_three(n,s,idx,N):
+    global arr
+    if n == 0:
+        arr.append(s)
+        return
+    if idx == N:
+        return
+    one_two_three(n-1,s+[1],idx+1,N)
+    one_two_three(n - 2,s+[2],idx+1,N)
+    one_two_three(n - 3,s+[3],idx+1,N)
+
+
+T = int(input())
+for i in range(T):
+    n = int(input())
+    arr = []
+    one_two_three(n, [], 0, n)
+    print(len(arr))
+
+'''
+# 다이내믹 프로그래밍
 T = int(input())
 d=[0] * 12
 d[1]=1
@@ -9,3 +31,4 @@ for i in range(4,12):
 for _ in range(T):
     n = int(input())
     print(d[n])
+'''
