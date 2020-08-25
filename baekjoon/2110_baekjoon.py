5c1d8d336a77e74b195095e024e330efcc3ea153
eshc@naver.com <eshc@naver.com>
2020-08-26 01:27:10 +0900

    2110 clear

--- baekjoon/2110_baekjoon.py
+++ baekjoon/2110_baekjoon.py
@@ -1,27 +1,23 @@
-def binary(index, ci,cnt,d):
-    global l,r,mid,m
-    if index>=len(dist) or ci>len(dist):
-        if cnt<C:
-            r = mid-1
-            return
-        else:
-            m=mid
-            l = mid + 1
-            return
-    s = sum(dist[index:ci])
-    if s >= d:
-        binary(ci,ci+1,cnt+1,d)
-    else:
-        binary(index,ci+1,cnt,d)
+import sys
 
-N, C = map(int,input().split())
-router = [ int(input()) for i in range(N)]
+N, C = map(int,sys.stdin.readline().split())
+router = [ int(sys.stdin.readline()) for i in range(N)]
 router.sort()
-m=0
-dist = [router[i+1]-router[i] for i in range(N-1) ]
-l, r = min(router),max(router)
+l, r = 1,max(router)
 while l<=r:
     mid = (r+l)//2
-    binary(0,1,1,mid)
+    cnt = 1
+    cp = router[0]
+    for i in range(1,N):
+        if mid+cp<=router[i]:
+            cnt+=1
+            cp=router[i]
 
-print(m)
+    if cnt < C :
+
+        r = mid-1
+    else:
+        a = mid
+        l = mid+1
+
+print(a)
