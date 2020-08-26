c2dfd0ea23dd715b70dd942e9a049aaddd2695f3
eshc@naver.com <eshc@naver.com>
2020-08-26 23:45:57 +0900

    1939 clear

--- baekjoon/1939_baekjoon.py
+++ baekjoon/1939_baekjoon.py
@@ -1,16 +1,37 @@
 import collections
+import sys
 
-N, M = map(int,input().split())
-B = [[] for _ in range(M+1)]
-visited = [ 0 for _ in range(M+1)]
-queue = collections.deque()
+def bfs(w):
+    visited = [0 for _ in range(N + 1)]
+    queue = collections.deque()
+    queue.append(s)
+    while queue:
+        q = queue.popleft()
+        visited[q]=1
+        for el in B[q]:
+            if visited[el[0]]==0 and el[1]>=w:
+                visited[el[0]] = 1
+                queue.append(el[0])
+    if visited[e]==1:
+        return True
+    else:
+        return False
+
+
+N, M = map(int,sys.stdin.readline().split())
+B = [[] for _ in range(N+1)]
 for i in range(M):
-    a,b,c = map(int,input().split())
+    a,b,c = map(int,sys.stdin.readline().split())
     B[a].append([b,c])
     B[b].append([a,c])
-s,e = map(int,input().split())
-queue.append(s)
-while queue:
-    
-
-#b = [list(map(int,input().split())) for i in range(M)]
+s,e = map(int,sys.stdin.readline().split())
+l,r = 1,1000000000
+ans = l
+while l<=r:
+    mid = (l+r)//2
+    if bfs(mid):
+        ans = mid
+        l=mid+1
+    else:
+        r=mid-1
+print(ans)
