81ffa416e214434c22ef7c30c83dc6ceec7d667b
eshc@naver.com <eshc@naver.com>
2020-09-02 15:39:07 +0900

    11053 clear

--- baekjoon/11053_baekjoon.py
+++ baekjoon/11053_baekjoon.py
@@ -1,3 +1,15 @@
 T = int(input())
 arr = list(map(int,input().split()))
+arr2 = [1 for _ in range(T)]
+l,r = 0,0
+while l<T:
+    if T==1:
+        break
+    r+=1
+    if r<T and arr[l]<arr[r] :
+        arr2[r]=max(arr2[l]+1,arr2[r])
+    if r==T-1:
+        r = l
+        l+=1
 
+print(max(arr2))
